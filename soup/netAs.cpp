d848e11a8a62332838056c20a8dca3a04b9e8ad3
Alessandro <66976091+alessandromrc@users.noreply.github.com>
2022-12-27 02:57:10 +0100

    Added Ginernet Hosting Detection
    More infos about Ginernet:
    https://www.peeringdb.com/net/12386
    
    Example IP:
    https://iphub.info/?ip=188.213.7.0

--- soup/netAs.cpp
+++ soup/netAs.cpp
@@ -95,7 +95,8 @@ namespace soup
 		case 206444: // KUBBUR
 		case 140952: // Strong Technology, LLC
 		case 42473: // ANEXIA Internetdienstleistungs GmbH		
-			// Green Floid LLC
+		case 59432: // GINERNET S.L.
+				// Green Floid LLC
 		case 40622:
 		case 204957:
 			// Nuclearfallout Enterprises, Inc (NFO)
